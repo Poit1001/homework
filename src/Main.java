5f5df0bace2a657c0fd6846c59b4d0b74ca13aef
Max <maksimperfilov2@gmail.com>
2023-10-01 18:53:05 +0300

    new hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        in.useLocale(Locale.US);
+        double x = in.nextDouble();
+        boolean res = 3 <= x && x <= 8;
+        System.out.println(res);
     }
 }
