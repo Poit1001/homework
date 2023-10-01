d6224a71b3c4cf1cf14e9c5372f9036e031e31e3
Max <maksimperfilov2@gmail.com>
2023-10-01 18:56:27 +0300

    new hw

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         in.useLocale(Locale.US);
         double x = in.nextDouble();
-        boolean res = -3 <= x && x <= 5 || 9 <= x && x <= 15;
+        boolean res = -2 <= x && x <= 3 || 6 <= x && x <= 9;
         System.out.println(res);
     }
 }
