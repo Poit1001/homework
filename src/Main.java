8e57b623e6658c54e3973b0f62c3ee9f8d5c3dfe
Max <maksimperfilov2@gmail.com>
2023-10-01 19:30:28 +0300

    new hw

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         in.useLocale(Locale.US);
-        double x = in.nextDouble();
-        boolean res = 100 <= x && x <= 999 && x % 5 == 0;
+        int x1 = in.nextInt(), x2 = in.nextInt(), x3 = in.nextInt(), x4 = in.nextInt();
+        boolean res = x1==-x2 || x1 == -x3 || x1 == -x4 ||  x2 == -x3 || x2 == -x4 || x3 == -x4;
         System.out.println(res);
     }
 }
