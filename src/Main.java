ab4c54bb1d2fc711fd1c6fd63be70f378c8f9602
Max <maksimperfilov2@gmail.com>
2023-10-02 19:54:10 +0300

    new hw

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        in.useLocale(Locale.US);
-        int x1 = in.nextInt(), x2 = in.nextInt(), x3 = in.nextInt();
-        boolean res = (x1 % 2) == 0 && (x2 % 2 == 0 || x3 % 2 == 0) || (x2 % 2 == 0 && x3 % 2 == 0);
-        System.out.println(res);
+        int x = in.nextInt();
+        if (0 <= x && x <=9) System.out.println("DIGIT");
+        else if (10 <= x && x <= 99) System.out.println("NUM");
+        else System.out.println("OTHER");
     }
 }
