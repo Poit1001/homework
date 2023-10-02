4b1846ad1217c468663f4739c424410a7f3c0ee3
Max <maksimperfilov2@gmail.com>
2023-10-02 20:24:26 +0300

    new hw

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int x = in.nextInt(), x1 = in.nextInt(), x2 = in.nextInt();
-        if (x >= x1 && x >= x2) {
-            if (x1>=x2) System.out.println(x2 + " " + x1 + " " + x);
-            else System.out.println(x1 + " " + x2 + " " + x);
-            }
-        else if (x1 >= x && x1 >= x2) {
-            if (x >= x2) System.out.println(x2 + " " + x + " " + x1);
-            else System.out.println(x + " " + x2 + " " + x1);
-            }
-        else {
-            if (x >= x1) System.out.println(x1 + " " + x + " " + x2);
-            else System.out.println(x + " " + x1 + " " + x2);
-            }
-        }
+        int x = in.nextInt();
+        if (0 <= x && x <=9) System.out.println("DIGIT");
+        else if (10 <= x && x <= 99) System.out.println("NUM");
+        else System.out.println("OTHER");
     }
+}
